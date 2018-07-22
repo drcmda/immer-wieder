069d5115db43d087c01a0939fa1641d54bdf383d
Paul Henschel <drcmda@gmail.com>
2018-07-22 22:11:49 +0200

    simpler demo + banner

--- assets/example.js
+++ assets/example.js
@@ -1,45 +1,43 @@
 import createContext from 'immer-wieder'
 
 const { Provider, Consumer } = createContext(setState => ({
-  // This is the default state
-  users: {
-    1: { id: 1, name: 'No-Name' },
+  // Everything in here is your state
+  bands: {
+    0: { name: 'Flipper' },
+    1: { name: 'Melt Banana' },
   },
-  ids: [1],
-
-  // Actions can go right into state, or wrap them in further objects ...
-  setUserName: (id, name) =>
-    // setState behaves like generic setState with immer semantics
-    setState(draft => {
-      // You can mutate the draft
-      draft.users[id].name = name
-      // Or return a reduced shallow clone of state like always
-      // return { ...draft, users: { ...draft.users, [id]: { ...draft.users[id], name } } }
-    }),
+  ids: [0, 1],
+  // ... including actions
+  // You can wrap and nest them, too, makes it easier to access them later ...
+  actions: {
+    changeName: (id, name) =>
+      // setState behaves like generic setState with immer semantics
+      setState(draft => {
+        // You can mutate the draft
+        draft.bands[id].name = name
+        // Or return a reduced shallow clone of state like always
+        // return { ...draft, users: { ...draft.users, [id]: { ...draft.users[id], name } } }
+      }),
+  },
 }))
 
-const User = ({ id }) => (
-  <Consumer
-    // Select is optional, if present the component renders only on specific state changes
-    // Actions can be fetched right from the store
-    select={({ users, setUserName }) => ({ ...users[id], setUserName })}>
-    {({ name, setUserName }) => (
+const EditDetails = ({ id }) => (
+  // Select is optional, if present the component renders only when the state you select changes
+  // Actions can be fetched right from the store
+  <Consumer select={store => ({ ...store.bands[id], ...store.actions })}>
+    {({ name, changeName }) => (
       <div>
-        <input value={name} onChange={e => setUserName(id, e.target.value)} />
-        {name}
+        <h1>{name}</h1>
+        <input value={name} onChange={e => changeName(id, e.target.value)} />
       </div>
     )}
   </Consumer>
 )
 
-const Users = () => (
-  <Consumer select={store => store.ids}>
-    {ids => ids.map(id => <User key={id} id={id} />)}
-  </Consumer>
-)
-
 const App = () => (
   <Provider>
-    <Users />
+    <Consumer select={store => store.ids}>
+      {ids => ids.map(id => <EditDetails key={id} id={id} />)}
+    </Consumer>
   </Provider>
 )

e6087515318c9ee0021c0900c1f2900ba992eebc
Paul Henschel <drcmda@gmail.com>
2018-08-28 10:33:15 +0200

    babel 7

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,25 @@
 import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
-export default {
-  input: 'src/index.js',
-  output: [
-    { file: `${pkg.main}.js`, format: 'cjs' },
-    { file: `${pkg.module}.js`, format: 'es' },
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [babel()],
-}
+const root = process.platform === 'win32' ? path.resolve('/') : '/'
+const external = id => !id.startsWith('.') && !id.startsWith(root)
+const getBabelOptions = ({ useESModules }) => ({
+  exclude: '**/node_modules/**',
+  runtimeHelpers: true,
+  plugins: [['@babel/transform-runtime', { regenerator: false, useESModules }]],
+})
+
+export default [
+  {
+    input: 'src/index.js',
+    output: { file: `${pkg.module}.js`, format: 'esm' },
+    external,
+    plugins: [babel(getBabelOptions({ useESModules: true }))],
+  },
+  {
+    input: 'src/index.js',
+    output: { file: `${pkg.main}.js`, format: 'cjs' },
+    external,
+    plugins: [babel(getBabelOptions({ useESModules: false }))],
+  },
+]
